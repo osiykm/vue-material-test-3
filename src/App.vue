ac9c1f4ae77d8734990ae739d00a53093ba2a85a
osiykm <osiykmukola@gmail.com>
2019-07-28 07:13:57 +0300

    add axios

--- src/App.vue
+++ src/App.vue
@@ -17,21 +17,6 @@
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    color: #42b983;
-    font-weight: bold;
-    &.router-link-exact-active {
-      color: #7299c0;
-    }
-  }
 }
 
 </style>
